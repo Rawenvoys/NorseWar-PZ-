11ca23206404a0f0e210706da2c5a3257c19de86
Rawenvoys <lukwis95@gmail.com>
2016-10-25 11:27:57 +0200

    Rejestracja i logowanie

--- Gra/Gra/Controllers/HomeController.cs
+++ Gra/Gra/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using Gra.Helper;
+using Gra.Models;
 
 namespace Gra.Controllers
 {
@@ -10,6 +14,57 @@ namespace Gra.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Account account)
+        {
+            //using (var sC = Methods.GetSqlConnection(Consts.DATABASE))
+            //{
+            //    if (sC == null)
+            //        return View();
+            //    return View();
+
+            //}
+            using (AccountDbContext aDC = new AccountDbContext())
+            {
+                var user = aDC.accountContext.Single(u => u.Login == account.Login && u.Password == account.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", Consts.LOGIN_ERROR);
+                    return View();
+                }
+                Session[Consts.SESSION_ID] = user.Id.ToString();
+                Session[Consts.SESSION_LOGIN] = user.Login.ToString();
+                return RedirectToAction("Main");
+            }
+
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Account account)
+        {
+            if (!ModelState.IsValid)
+                ModelState.Clear();
+            using (AccountDbContext aDC = new AccountDbContext())
+            {
+                aDC.accountContext.Add(account);
+                aDC.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            if (Session[Consts.SESSION_ID] == null)
+                return RedirectToAction("Index");
             return View();
         }
     }
---
+++ Gra/Gra/Models/AccountDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Gra.Models
+{
+    public class AccountDbContext : DbContext
+    {
+        public DbSet<Account> accountContext { get; set; }
+    }
+}
