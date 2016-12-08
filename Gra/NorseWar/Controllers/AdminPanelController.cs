e78c101987e623230eb915a20eb41be23d1bdbdd
Rawenvoys <lukwis95@gmail.com>
2016-12-08 06:08:10 +0100

    Dodany Panel Admin Controllers

---
+++ Gra/NorseWar/Controllers/AdminPanelController.cs
@@ -0,0 +1,53 @@
+﻿using NorseWar.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorseWar.Models.ViewModel;
+
+//Czyli, 
+//+wypisać wszystkie itemy w grze
+//-dodaj item do bazy danych
+//-wypisać wszystkich graczy
+//-edytuj nazwę gracza
+//-pokaż info o graczu?
+//-napisz @ do wsyzstkich
+//-banuj
+
+namespace NorseWar.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private GameContext db = new GameContext();
+        // GET: AdminPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Items()
+        {
+            //List<ItemViewModel> items = new List<ItemViewModel>();
+            //items.Add();
+            ItemViewModel items = new ItemViewModel();
+            var weapons = db.ItemWeapons.ToList();
+            foreach (var weapon in weapons)
+            {
+                items.weapons.Add(weapon);
+            }
+            var shields = db.ItemShields.ToList();
+            foreach (var shield in shields)
+            {
+                items.shields.Add(shield);
+            }
+
+            return View(items);
+        }
+
+        public ActionResult ChangeNickname()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gra/NorseWar/Models/ViewModels/ItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorseWar.Models.ViewModel
+{
+    public class ItemViewModel
+    {
+        public List<ItemAccessory> accessories = new List<ItemAccessory>();
+        public List<ItemArmor> armors = new List<ItemArmor>();
+        public List<ItemBoots> boots = new List<ItemBoots>();
+        public List<ItemHelmet> helmets = new List<ItemHelmet>();
+        public List<ItemLegs> legss = new List<ItemLegs>();
+        public List<ItemShield> shields = new List<ItemShield>();
+        public List<ItemWeapon> weapons = new List<ItemWeapon>();
+    }
+}
