9cfeaaf9d8c08aec2fe45312a33c6073028930e5
pigeon <pigeon@DESKTOP-38B1R42>
2017-01-30 21:22:08 +0100

    User Controller fix

---
+++ Gra/NorseWar/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorseWar.Models;
+using NorseWar.Models.DAL;
+
+namespace NorseWar.Controllers
+{
+    public class UserController : Controller
+    {
+        private GameContext db = new GameContext();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var accounts = db.Accounts.Include(a => a.Stats);
+            return View(accounts.ToList());
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
