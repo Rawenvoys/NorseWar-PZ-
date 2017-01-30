07dc53818975ec0b4f2f2969de62036cf218b801
orzechovaty <orzechovaty@gmail.com>
2017-01-30 19:11:17 +0100

    jebać ruchać

---
+++ Gra/Gra/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using Gra.Helper;
+using Gra.Models;
+
+namespace Gra.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Account account)
+        {
+            //using (var sC = Methods.GetSqlConnection(Consts.DATABASE))
+            //{
+            //    if (sC == null)
+            //        return View();
+            //    return View();
+
+            //}
+            using (Model1 aDC = new Model1())
+            {
+                var user = aDC.Accounts.Single(u => u.Login == account.Login && u.Password == account.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", Consts.LOGIN_ERROR);
+                    return View();
+                }
+                Session[Consts.SESSION_ID] = user.Id.ToString();
+                Session[Consts.SESSION_LOGIN] = user.Login.ToString();
+                return RedirectToAction("Main");
+            }
+
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Accounts acc)
+        {
+            if (!ModelState.IsValid)
+                ModelState.Clear();
+            using (Model1 aDC = new Model1())
+            {
+                aDC.Accounts.Add(acc);
+                aDC.SaveChanges();
+            }
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            if (Session[Consts.SESSION_ID] == null)
+                return RedirectToAction("Index");
+            return View();
+        }
+    }
+}
---
+++ Gra/Gra/Helper/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Gra.Helper
+{
+    public class Methods
+    {
+        public static SqlConnection GetSqlConnection(string name)
+        {
+            var sC = new SqlConnection(ConfigurationManager.ConnectionStrings[name].ConnectionString);
+            return sC;
+        }
+    }
+}
