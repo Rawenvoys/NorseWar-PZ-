9164df247faf6f2d9a27eead7fba84537a0a8057
mzmiejko1994 <m.zmiejko1994@gmail.com>
2017-03-29 21:16:24 +0200

    textarea poczta

--- Gra/NorseWar/Models/Message.cs
+++ Gra/NorseWar/Models/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,7 @@ namespace NorseWar.Models
         public int RecipentId { get; set; }
 
         public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Text { get; set; }
 
         public DateTime Date { get; set; }
